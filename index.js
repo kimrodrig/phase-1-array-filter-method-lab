918d56eacde5611c831b892af684ec4fd611f873
Kim <rodrigkim@gmail.com>
2022-08-30 14:11:37 -0400

    commit tree 87badd7bab08741338febd84761e35d6b75be61d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter((name) => {
+        return name.toLowerCase() == string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(array, string){
+    const length = string.length;
+    return array.filter((name) => {
+        return name.slice(0,length).toLowerCase() == string.toLowerCase()
+    })
+}
+
+function matchName(array, string){
+    return array.filter((driver) => {
+        return driver["name"].toLowerCase() == string.toLowerCase()
+    })
+}
